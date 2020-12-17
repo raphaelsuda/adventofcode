ebbddbad81d3ed44ec7be7a43c11937c89942a14
Raphael Suda <rsuda@lws75.imws.tuwien.ac.at>
2020-12-17 09:23:02 +0100

    Day 17

---
+++ 17b.jl
@@ -0,0 +1,25 @@
+initial_config(data) =  Set((x, y, 0, 0) for x in 1:size(data, 2), y in 1:size(data, 1) if data[y,x] == '#')
+
+get_neighbors((x, y, z, w)) = Set((i, j, k, l) for i in x-1:x+1, j in y-1:y+1, k in z-1:z+1, l in w-1:w+1 if (i, j, k, l) != (x, y, z, w))
+
+function check_candidate(coords, active_cubes)
+    n_active = count(n ∈ active_cubes for n in get_neighbors(coords))
+    (coords ∈ active_cubes && n_active ∈ Set([2,3])) && return true
+    (coords ∉ active_cubes && n_active == 3) && return true
+    return false
+end
+
+function solve17b(data, cycles=6)
+    active_cubes = initial_config(data)
+    new_active_cubes = Set()
+    candidates = reduce(∪, get_neighbors(c) for c in active_cubes) ∪ active_cubes
+    for i in 1:cycles
+        new_active_cubes = Set(c for c in candidates if check_candidate(c, active_cubes))
+        active_cubes = new_active_cubes
+        candidates = reduce(∪, get_neighbors(c) for c in active_cubes) ∪ active_cubes
+    end
+    return length(new_active_cubes)
+end
+
+data = reduce(vcat, permutedims.(collect.(readlines("17.input"))))
+solve17b(data)
