dd4ad9c40c48a3b4ed16a151beae4969439f24d5
Raphael Suda <rsuda@lws75.imws.tuwien.ac.at>
2020-12-18 08:27:00 +0100

    Day 18

---
+++ 18a.jl
@@ -0,0 +1,12 @@
+⋅(x,y) = x * y
+×(x,y) = x + y
+
+function evaluate(s,rep)
+    for k in keys(rep)
+        s = replace(s, k=>rep[k])
+    end
+    return eval(Meta.parse(s))
+end
+
+replace_characters = Dict("*"=>"⋅", "+"=>"×")
+reduce(+,evaluate(l, replace_characters) for l in readlines("18.input"))
